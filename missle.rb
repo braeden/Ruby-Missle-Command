734d234cd8cc9a71aed3bb4450b25b15b0f8b408
Braeden123 <braedensmith2@gmail.com>
2015-06-13 20:26:09 -0400

    commit

--- missle.rb
+++ missle.rb
@@ -2,14 +2,17 @@ require 'ray'
 Ray.game "Missle Command", :size => [800, 600] do
   register { add_hook :quit, method(:exit!) }
   scene :square do
-    @missle = Ray::Polygon.circle([200, 40], 5, Ray::Color.red)
-    @missle.pos = [100,100]
-    
+    @missles = 4.times.map do
+      m = Ray::Polygon.circle([200, 40], 5, Ray::Color.red)
+      m.pos = [rand(0...800),rand(-50...-10)]
+    end
     always do
-
+      @missles.each do
+        @missle.pos += [0,2]
+      end
     end
     render do |win|
-      win.draw @missle
+      win.draw @missles.each
     end
   end
   scenes << :square
